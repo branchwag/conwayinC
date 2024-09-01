8f6b3208c129e17cb29753ec9977483fd94c5965
branchwag <branchleannelmb@gmail.com>
2024-08-31 22:36:41 -0400

    gets game of life working

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A while back, I followed a tutorial to put together Conway's game of life in C++ using raylib. This time I thought I would do it in C:
 
+To compile:  
 ```
 gcc -o gameoflife gameoflife.c -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
 ```
+
+The rules of Conway's Game of Life:
+
+Any live cell with 2 or 3 live neighbors survives.
+Any dead cell with exactly 3 live neighbors becomes a live cell.
+All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+Press the spacebar to reset the grid with a new random configuration.
--- gameoflife.c
+++ gameoflife.c
@@ -1,36 +1,64 @@
 #include "raylib.h"
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define WINDOW_WIDTH 750
 #define WINDOW_HEIGHT 750
-#define FPS 12
-#define CELL_SIZE 25
-#define LINE_THICKNESS 2
+#define FPS 10
+#define CELL_SIZE 15
+#define LINE_THICKNESS 1
 #define GRID_WIDTH (WINDOW_WIDTH / CELL_SIZE)
 #define GRID_HEIGHT (WINDOW_HEIGHT / CELL_SIZE)
 
 int grid[GRID_HEIGHT][GRID_WIDTH];
+int temp_grid[GRID_HEIGHT][GRID_WIDTH];
 
 void initializeGrid() {
     for (int y = 0; y < GRID_HEIGHT; y++) {
         for (int x = 0; x < GRID_WIDTH; x++) {
-            grid[y][x] = rand() % 2;  // Randomly set each cell to 0 or 1
+            grid[y][x] = rand() % 4 == 0;  // 25% chance of being alive
         }
     }
 }
 
-void drawGrid() {
-    // Draw filled cells
+int countNeighbors(int x, int y) {
+    int count = 0;
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            if (i == 0 && j == 0) continue;
+            int nx = (x + i + GRID_WIDTH) % GRID_WIDTH;
+            int ny = (y + j + GRID_HEIGHT) % GRID_HEIGHT;
+            count += grid[ny][nx];
+        }
+    }
+    return count;
+}
+
+void updateGrid() {
+    memcpy(temp_grid, grid, sizeof(grid));
     for (int y = 0; y < GRID_HEIGHT; y++) {
         for (int x = 0; x < GRID_WIDTH; x++) {
-            if (grid[y][x] == 1) {
+            int neighbors = countNeighbors(x, y);
+            if (grid[y][x]) {
+                temp_grid[y][x] = (neighbors == 2 || neighbors == 3);
+            } else {
+                temp_grid[y][x] = (neighbors == 3);
+            }
+        }
+    }
+    memcpy(grid, temp_grid, sizeof(grid));
+}
+
+void drawGrid() {
+    for (int y = 0; y < GRID_HEIGHT; y++) {
+        for (int x = 0; x < GRID_WIDTH; x++) {
+            if (grid[y][x]) {
                 DrawRectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE, GREEN);
             }
         }
     }
 
-    // Draw grid lines
     for (int x = 0; x <= WINDOW_WIDTH; x += CELL_SIZE) {
         DrawLineEx((Vector2){x, 0}, (Vector2){x, WINDOW_HEIGHT}, LINE_THICKNESS, LIGHTGRAY);
     }
@@ -42,7 +70,7 @@ void drawGrid() {
 }
 
 int main() {
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Conway's Game of Life");
     SetTargetFPS(FPS);
     
     srand(time(NULL));
@@ -51,11 +79,15 @@ int main() {
     Color grey = {29, 29, 29, 255};
     
     while (!WindowShouldClose()) {
-        BeginDrawing();
+        if (IsKeyPressed(KEY_SPACE)) {
+            initializeGrid();  
+        }
         
+        updateGrid();
+        
+        BeginDrawing();
         ClearBackground(grey);
         drawGrid();
-        
         EndDrawing();
     }
     
